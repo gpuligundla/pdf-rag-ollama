fc1cd704edb860e815f23f093d9947ca282c13a9
geethakrishna puligundla <gpuligu@clemson.edu>
2025-06-19 12:16:52 -0400

    added base structure for the streamlit

---
+++ src/main.py
@@ -0,0 +1,91 @@
+"""
+The Main module of the Ollama RAG-based PDF chat application
+"""
+
+import streamlit as st
+import logging
+import os
+import tempfile
+import shutil
+import pdfplumber
+import ollama
+import warnings
+
+warnings.filters('ignore', category=UserWarning, message='.*torch.classes.*')
+
+from langchain_community.document_loaders import UnstructuredPDFLoader
+from langchain_ollama import OllamaEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.prompts import ChatPromptTemplate, PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_ollama import ChatOllama
+from langchain_core.runnables import RunnablePassthrough
+from langchain.retrievers.multi_query import MultiQueryRetriever
+
+
+# Set protobuf environment variable to avoid error messages
+# This might cause some issues with latency but it's a tradeoff
+os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"
+
+# Define persistent directory for ChromaDB
+PERSIST_DIRECTORY = os.path.join("data", "vectors")
+
+# Streamlit page configuration
+st.set_page_config(
+    page_title="Ollama PDF RAG Streamlit UI",
+    page_icon="🎈",
+    layout="wide",
+    initial_sidebar_state="collapsed",
+)
+
+# Logging configuration
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+logger = logging.getLogger(__name__)
+
+def extract_model_names(models_info):
+    """Extract the model name"""
+    try:
+        if hasattr(models_info, 'models'):
+            model_names = tuple(model.model for model in models_info.models)
+        else:
+            model_names = tuple()
+        logger.info(f"Extracted model names: {model_names}")
+        return model_names
+    except Exception as e:
+        logger.error(f"Error extracting model names: {model_names}")
+        return tuple()
+    
+def main():
+    st.subheader("🧠 Ollama PDF RAG playground", divider="gray", anchor=False)
+
+    models_info = ollama.list()
+    available_models = extract_model_names(models_info)
+
+    # Create layout
+    col1, col2 = st.columns([1.5, 2])
+
+     # Initialize session state
+    if "messages" not in st.session_state:
+        st.session_state["messages"] = []
+    if "vector_db" not in st.session_state:
+        st.session_state["vector_db"] = None
+    if "use_sample" not in st.session_state:
+        st.session_state["use_sample"] = False
+    
+    # Model selection
+    if available_models:
+        selected_model = col2.selectbox(
+            "Pick a model available locally on your system ↓", 
+            available_models,
+            key="model_select"
+        )
+
+
+
+if __name__ == "__main__":
+    main()
